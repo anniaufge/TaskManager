043721055a63f9839b40bff86d182e65245397f7
Annika Aufgebauer <annikaaufge@gmail.com>
2026-04-23 17:37:05 -0500

    add task model and enums

---
+++ TaskManager/src/model/Task.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Task {
+	private String title;
+	private String description;
+	private Priority priority;
+	private TaskType type;
+	private String status;
+	private LocalDate deadline;
+	
+	public Task(String title, String description, Priority priority, TaskType type, LocalDate deadline) {
+		this.title = title;
+		this.description = description;
+		this.priority = priority;
+		this.type = type;
+		this.status = "TODO";
+		this.deadline = deadline;
+	}
+
+	// getters
+	public String getTitle() { return title; }
+	public String getDescription() { return description; }
+	public Priority getPriority() { return priority; }
+	public TaskType getType() { return type; }
+	public String getStatus() { return status; }
+	public LocalDate getDeadline() { return deadline; }
+	
+	// setters
+	public void setTitle(String title) { this.title = title; }
+	public void setDescription(String description) { this.description = description; }
+	public void setPriority(Priority priority) { this.priority = priority; }
+	public void setType(TaskType type) {this.type = type; }
+	public void setStatus(String status) { this.status = status; }
+	public void setDeadline(LocalDate deadline) { this.deadline = deadline; }
+	
+	@Override
+	public String toString() {
+		return title + " {" + type + "] - " + priority + " - " + status;
+	}
+}
